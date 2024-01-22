8fe1a34c0c20294f1608c09c0554467ffa3bf9a1
Brandon Peterson <brandonacfuntech@gmail.com>
2024-01-22 00:03:53 -0600

    Updated UI, added quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import { ActionBtn } from "@/components";
+import { ActionBtn, Quote } from "@/components";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { TbChevronsUpRight, TbUser, TbSend } from "react-icons/tb";
+import {
+  TbChevronsUpRight,
+  TbUser,
+  TbSend,
+  TbArrowNarrowRight,
+  TbTools,
+} from "react-icons/tb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,12 +19,12 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <main>
+      <main className="space-y-20">
         <section className="flex flex-row mt-20 space-x-10 justify-center items-center max-lg:flex-col max-lg:space-x-0 max-lg:mt-10">
           <div>
             <div className="w-52 h-52 overflow-hidden rounded-full">
               <Image
-                src="/BrandonPeterson.jpg"
+                src="/BrandonPeterson.webp"
                 alt="Brandon Peterson"
                 width={1000}
                 height={1000}
@@ -50,23 +56,37 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="flex my-40 justify-center space-x-10 max-lg:flex-col max-lg:space-x-0 max-lg:space-y-10 max-lg:my-10">
+        <Quote
+          quote="Life is like riding a bicycle. To keep your balance, you must keep moving."
+          author="Albert Einstein"
+        />
+        <section className="flex my-10 justify-center space-x-10 max-lg:flex-col max-lg:space-x-0 max-lg:space-y-10 max-lg:my-10">
           <Link
             href="/about"
-            className="flex flex-col w-full items-center p-[5px] animate-text rounded-xl text-center text-2xl bg-gradient-to-r from-teal-500 to-purple-500 hover:scale-110 ease-in-out duration-300"
+            className="group flex h-full flex-col w-full items-center p-[5px] animate-text rounded-xl text-center text-2xl bg-gradient-to-r from-teal-500 to-purple-500"
           >
-            <div className="flex flex-col w-full items-center h-full bg-c1 p-10 rounded-lg space-y-5 ">
-              <TbUser size={70} />
-              <h3 className="">More about me</h3>
+            <div className="flex w-full items-center justify-center bg-c1 p-5 rounded-lg space-x-5">
+              <div className="flex w-full items-center justify-center space-x-5 group-hover:-translate-x-6 ease-in-out duration-300">
+                <TbUser className="w-10 h-10" />
+                <h3 className="">More about me</h3>
+              </div>
+              <div className="absolute translate-x-28 opacity-0 group-hover:opacity-100 ease-in-out duration-300">
+                <TbArrowNarrowRight className="w-10 h-10" />
+              </div>
             </div>
           </Link>
           <Link
             href="/projects"
-            className="flex flex-col w-full items-center p-[5px] animate-text rounded-xl text-center text-2xl bg-gradient-to-r from-c5  to-c11 hover:scale-110 ease-in-out duration-300"
+            className="group flex h-full flex-col w-full items-center p-[5px] animate-text rounded-xl text-center text-2xl bg-gradient-to-r from-c5  to-c11"
           >
-            <div className="flex flex-col w-full items-center h-full bg-c1 p-10 rounded-lg space-y-5">
-              <TbSend size={70} />
-              <h3 className="">See my projects</h3>
+            <div className="flex w-full items-center justify-center bg-c1 p-5 rounded-lg space-x-5">
+              <div className="flex w-full items-center justify-center space-x-5 group-hover:-translate-x-6 ease-in-out duration-300">
+                <TbTools className="w-10 h-10" />
+                <h3 className="">See my projects</h3>
+              </div>
+              <div className="absolute translate-x-28 opacity-0 group-hover:opacity-100 ease-in-out duration-300">
+                <TbArrowNarrowRight className="w-10 h-10" />
+              </div>
             </div>
           </Link>
         </section>
